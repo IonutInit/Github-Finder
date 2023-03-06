8a3bcd6ffa587d33fce449f5b2afcee40db9a960
Ionut Cojocaru <111282912+IonutInit@users.noreply.github.com>
2023-03-06 16:33:49 +0000

    started project

---
+++ src/components/users/UserItem.tsx
@@ -0,0 +1,7 @@
+function UserItem({user}) {
+  return (
+    <div>{user.login}</div>
+  )
+}
+
+export default UserItem
---
+++ src/components/users/UserResults.tsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react'
+import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
+
+type Users = {
+    [key: string]: string | number
+    //the above is an index signature which handles each key in a generalised manner
+    // login: string
+}
+
+function UserResults() {
+    const [users, setUsers] = useState<Users[]>([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+       fetchUsers() 
+    }, [])
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`
+            }
+        })
+
+        const data = await response.json()
+    
+        setUsers(data)
+        setLoading(false)
+    }
+
+    if(!loading) {
+    return (
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+            <UserItem key={user.id} user={user}/>
+        ))}
+    </div>
+  )
+    } else {
+        <Spinner />
+    }
+}
+
+export default UserResults
